3b5ef55a92b487937094ae146ac00d46aa5ee8ac
Arne Cuperus <cuperusarne@gmail.com>
2023-03-22 10:15:41 +0100

    Added example config.yaml

--- main.py
+++ main.py
@@ -4,21 +4,28 @@ import yaml
 
 access_token = ""
 
-with open("config.yaml", "r") as configuration_file:
-    config = yaml.safe_load(configuration_file)
+def load_config():
+    with open("config.yaml", "r") as configuration_file:
+        config = yaml.safe_load(configuration_file)
 
-if config == None:
-    raise ValueError("config.yaml is empty! Please fill in your credentials")
+    if config == None:
+        raise ValueError("config.yaml is empty! Please fill in your credentials")
 
-if "institution" in config and not config["institution"] == None:
-    instituion = config["institution"]
-else:
-    raise ValueError("institution not defined")
+    return config
+
+def get_variable(config, variable):
+    if variable in config and not config[variable] == None:
+        return config[variable]
+    else:
+        raise ValueError(f"{variable} not defined")
+
+configuration = load_config()
+institution = get_variable(configuration, "institution")
 
 current_year = datetime.date.today().year
 current_week = datetime.date.today().isocalendar()[1]
 
-client = Client(school=instituion)
+client = Client(school=institution)
 usercode = client.get_user(token=access_token)["response"]["data"][0]["code"]
 print(usercode)
 print (client.get_liveschedule(token=access_token, week=str(current_year)+str(current_week), usercode=int(usercode)))
